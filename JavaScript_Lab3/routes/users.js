e05f1466c9205bb4e23a8f514c3836643ca0c8ac
nadiarose1205 <200344428@student.georgianc.on.ca>
2020-06-16 20:28:38 -0400

    Final save

--- JavaScript_Lab3/routes/users.js
+++ JavaScript_Lab3/routes/users.js
@@ -4,7 +4,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-    res.send('respond with a resource');
+    res.render('user', { name: "Onalee" });
 });
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.render('user', { name: "Don" });
+});
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    res.render('user', { name: "Matthew" });
+}); 
 
 module.exports = router;
